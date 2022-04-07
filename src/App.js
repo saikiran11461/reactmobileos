792263aefaaa5eb53f464090bb0124178bd6f8af
saikiran11461 <70562454+saikiran11461@users.noreply.github.com>
2022-04-07 21:13:05 +0530

    initila

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-const Os = ["Services" ,"Projects", "About"];
+const Os = ["Services" ,"Projects", "About", "jani", "sai kiran"];
 function App() {
   return (
     <div className="App">
